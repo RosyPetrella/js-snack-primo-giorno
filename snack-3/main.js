65629194be07b34227d2db9d65303e477947109c
RosyPetrella <petrellamr@gmail.com>
2025-02-13 12:27:19 +0100

    edit

--- snack-3/main.js
+++ snack-3/main.js
@@ -4,7 +4,7 @@
 let somma = 0;
 
 for (let i = 1; i <= 10; i++) {
-  const number = parseFloat(prompt("Type a number"));
+  const number = Number(prompt("Type a number"));
   console.log(number);
   somma += number;
 }
