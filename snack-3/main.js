94f103a32f7c11b3b3af9157bb28eb1a40a645f9
RosyPetrella <petrellamr@gmail.com>
2025-02-13 12:03:45 +0100

    snack 3

---
+++ snack-3/main.js
@@ -0,0 +1,12 @@
+// Il software deve chiedere per 10 volte all’utente di inserire un numero.
+// Il programma stampa la somma di tutti i numeri inseriti.
+
+let somma = 0;
+
+for (let i = 1; i <= 10; i++) {
+  const number = parseInt(prompt("Type a number"));
+  console.log(number);
+  somma += number;
+}
+
+console.log(somma);
